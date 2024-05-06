152ea340f257abf0a75e978ce37d21cbd77a5728
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-06 15:12:13 +0200

    add named routes system

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,8 +2,8 @@
     <nav class="container">
         <div class="logo">Laravel</div>
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
+            <li><a href="{{route('home')}}">Home</a></li>
+            <li><a href="{{route('about')}}">About</a></li>
         </ul>
     </nav>
 </header>
